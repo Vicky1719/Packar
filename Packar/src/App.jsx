7f96d6a925b44c7dd7360022769b3b82a29f83f4
Vicky1719 <112522032+Vicky1719@users.noreply.github.com>
2023-02-01 15:55:23 +0100

    Clases

--- Packar/src/App.jsx
+++ Packar/src/App.jsx
@@ -1,17 +1,26 @@
-import { useCatFact } from './hooks/useCatFact'
+import { useCatFact } from "./hooks/useCatFact";
+import "./App.css";
 
-export default function App () {
-  const { fact, refreshRandomFact } = useCatFact()
+export default function App() {
+  const { fact, refreshRandomFact } = useCatFact();
 
   const handleClick = async () => {
-    refreshRandomFact()
-  }
+    refreshRandomFact();
+  };
 
   return (
     <main>
-      <h1>App de gatitos</h1>
-      <button onClick={handleClick}>Refresh</button>
-      {fact && <img src={fact} alt='image-cat' />}
+      <header>
+        <h1>Gatitolandia</h1>
+      </header>
+      <div className="content">
+      <container>
+        <button className="btn" onClick={handleClick}>Refresh</button>
+        <div className="random-image">
+        {fact && <img src={fact} alt="image-cat" height={400} width={600} />} 
+        </div>
+      </container>
+      </div>
     </main>
-  )
+  );
 }
